e3546a4f4ec3e3456f7a8b543004aaa494e54e68
Mukhtar101 <muktarmori@gmail.com>
2018-10-01 17:43:30 +0800

    Versi 1

---
+++ latihan.java
@@ -0,0 +1,21 @@
+
+public class latihan {
+
+	public static void main(String[] args) {
+	//	String mhs1 = "Wahida A";
+	//	String mhs2 = "Patimah";
+	//	String mhs3 = "Zulfahmi";
+		
+		String[]mhs = {"Wahida A", "Patimah A", "Zulfahmi A"};
+	//	for (int i=0;i<mhs.length;i++)System.out.println (mhs[i]);
+		
+		String [] [] nim__mhs = {
+				{ "D0217018", "Wahida A"},
+				{ "D0217019", "Patimah"},
+				{ "D0217022", "Zulfahmi"},
+				System.out.println
+		};
+		
+		
+		}
+	}
